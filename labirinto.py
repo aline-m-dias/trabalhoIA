a2b156d8185f2639b5926426580b630c1f3d9de7
Gultes <gustavoestevamsena@gmail.com>
2022-09-11 02:19:23 -0300

    Update coast-all-weight-A*-

--- labirinto.py
+++ labirinto.py
@@ -9,11 +9,12 @@ from scipy.spatial.distance import cityblock
 
 # Classe No com 3 atributos: estado, pai e ação
 class No():
-    def __init__(self, estado, pai, acao, noDestino):
+    def __init__(self, estado, pai, acao, noDestino, peso):
         self.estado = estado
         self.pai = pai
         self.acao = acao
         self.heuristica= cityblock(self.estado,noDestino)
+        self.peso = peso
        
         # print("Estado =", self.estado)
         # print("Pai =", self.pai)
@@ -30,7 +31,7 @@ class No():
             
     #f(n)= G(n)+ H(n)
     def funcaoAvaliacao(self):
-        f= self.custo() + self.heuristica
+        f= self.custo() + (self.peso * self.heuristica)
         return f
     
 # Classe para tratar Nós Fronteira
@@ -74,22 +75,12 @@ class FilaFronteira(PilhaFronteira):
             self.fronteira = self.fronteira[1:]
             return no
 
+# A Star Search(A*), usamos uma função de avaliação definida por
+# f(n)= g(n)+ wh(n) para gerenciamento da fronteira, onde g(n) 
+# representa o custo até aquele nó, w = o peso e h(n) a heurística 
+# admissível - que nunca superestima o custo real para resolver o problema
+
 class aStarFronteira(PilhaFronteira):
-    
-    # #g(n)
-    # def custo(self):
-    #     custo=0
-    #     no=self
-        
-    #     while no.pai != None:
-    #         custo=custo + 1
-    #         no=no.pai
-    #     return custo
-            
-    # #f(n)= G(n)+ H(n)
-    # def funcaoAvaliacao(self):
-    #     f= self.custo() + self.heuristica
-    #     return f
     
     def remove(self):
         if self.empty():
@@ -187,28 +178,15 @@ class Labirinto():
 
 
     # Invoca o método solve() para encontrar a solução 
-    def solve(self):
-        """Encontrar uma solução para labirinto, se existe."""
+    def solve(self,pesoh):
+        
 
         # Acompanhar o número de estados explorados
         self.num_explored = 0
 
         # Inicializa a fronteira apenas para o posição inicial
-       
-        inicio = No(estado=self.inicio, pai=None, acao=None, noDestino=self.objetivo)
-        
-        #escolhe o tipo da busca
-        print('''
-            Qual algoritmo  de busca deseja utilizar para encontrar a solução do labirinto?
-            
-            MENU:
-
-            [1] - Algoritmo de Busca em Largura
-            [2] - Algoritmo de Busca em Profundidade
-            [3] - Algoritmo de Busca A Estrela
-        ''')
-        opcao_escolhida = str(input('Escolha uma opção: '))
-    
+        inicio = No(estado=self.inicio, pai=None, acao=None, noDestino=self.objetivo, peso=pesoh)
+        # Define o método de gerenciamento de fronteira com base na opção escolhida
         if opcao_escolhida == '1':
             fronteira= FilaFronteira()
             print("Solucionando...")
@@ -221,7 +199,7 @@ class Labirinto():
         else:
             print("Valor inválido!!!")
             exit() 
-       
+        # Inicializa a fronteira apenas para o posição inicial
         fronteira.add(inicio)
      
         # Inicializa um conjunto vazio de estados não explorados
@@ -229,7 +207,7 @@ class Labirinto():
 
         # Mantem laço até encontrar solução
         while True:
-
+        
             # Se não sobrar nada na fronteira, então não há caminho
             if fronteira.empty():
                 raise Exception("sem solução")
@@ -240,6 +218,7 @@ class Labirinto():
 
             # Se o nó é objetivo, então temos uma solução
             if no.estado == self.objetivo:
+                self.custoTotal = no.custo()
                 acoes = []
                 celulas = []
                 while no.pai is not None:
@@ -257,7 +236,7 @@ class Labirinto():
             # Adiciona vizinhos a fronteira
             for acao, estado in self.vizinhos(no.estado):
                 if not fronteira.contem_estado(estado) and estado not in self.explored:
-                    filho = No(estado=estado, pai=no, acao=acao, noDestino=self.objetivo)
+                    filho = No(estado=estado, pai=no, acao=acao, noDestino=self.objetivo, peso = pesoh)
                     fronteira.add(filho)
 
     # Imprime o labirinto com os estados explorados
@@ -323,14 +302,44 @@ m = Labirinto(sys.argv[1])
 print("Labirinto: ")
 m.print()
 
-t1 = time.time()
-m.solve()
-t2 = time.time()
-tempo_execucao = t2 - t1 
-print("Tempo de Execução: ", tempo_execucao)
+"""Encontrar uma solução para labirinto, se existe."""
+#escolhe o tipo da busca
+print('''
+    Qual algoritmo  de busca deseja utilizar para encontrar a solução do labirinto?
+            
+        MENU:
 
-print("Estados Explorados:", m.num_explored)
-print("Solução: ")
-m.print()
-m.output_image("labirinto.png", show_explored=True)
+        [1] - Algoritmo de Busca em Largura
+        [2] - Algoritmo de Busca em Profundidade
+        [3] - Algoritmo de Busca A Estrela
+    ''')
 
+opcao_escolhida = str(input('Escolha uma opção: '))
+    
+if opcao_escolhida == '3':   
+    pesoh = [1, 2, 4, 8, 16, 32]
+    for p in pesoh:
+        t1 = time.time()  
+        m.solve(pesoh=p) 
+        t2 = time.time()
+        tempo_execucao = t2 - t1  
+        print("Busca em A*")
+        print("Para peso [w em f(n) = g(n) + wh(n)]: ", p)
+        print("Tempo de Execução: ", tempo_execucao)
+        print("Custo do Caminho;", m.custoTotal)
+        print("Estados Explorados:", m.num_explored)
+        print("Solução: ")
+        m.print()
+        m.output_image("labirinto.png", show_explored=True)
+else:
+    t1 = time.time()  
+    m.solve(pesoh=1) 
+    t2 = time.time()
+    tempo_execucao = t2 - t1
+    print("Busca em Largura/Profundidade")
+    print("Tempo de Execução: ", tempo_execucao)
+    print("Custo do Caminho;", m.custoTotal)
+    print("Estados Explorados:", m.num_explored)
+    print("Solução: ")
+    m.print()
+    m.output_image("labirinto.png", show_explored=True)
